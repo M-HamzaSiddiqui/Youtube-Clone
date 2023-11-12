4521bc808c7b2b8a4edba50554988fc43ba72903
M-HamzaSiddiqui <userhamza01@gmail.com>
2023-11-12 23:12:48 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,41 +1,22 @@
-// require ('dotenv').config({path: './env'})
-
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./env"
-})
+});
+
+const app = express();
 
 connectDB()
-
-
-
-
-
-
-/*
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
-
-const app = express()
-
-    ; (async () => {
-        try {
-            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-            app.on("errror", () => {
-                console.log("ERRR: ", error);
-                throw error;
-            });
-
-            app.listen(process.env.PORT, () => {
-                console.log(`App is listening on port ${process.env.PORT}`)
-            })
-
-        } catch (error) {
-            console.log("ERROR: ", error);
-            throw err;
-        }
-    });
-*/
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERRR: ", error)
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed: ", err);
+    })
